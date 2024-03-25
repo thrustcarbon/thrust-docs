b8bd5dc8cd9ec8289f7ea162f8dd29e0d60489bf
robbie@thrustcarbon.com <robbie@thrustcarbon.com>
2024-03-25 10:56:23 +0000

    Add Water segment

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjkwMjQ0/spectral.js?branch_slug=master&token=b9101bee-0690-4697-912d-ee821234d510";
+export default { extends: ruleset };
